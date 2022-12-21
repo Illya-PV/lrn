289d67d2b1b9a1147da817bc89441906f3297ea2
Ілля Павлов <ILLIA.PAVLOV@lnu.edu.ua>
2022-12-21 10:16:33 +0200

    first commit

---
+++ Learning.Dal/Models/MongoDBGoodsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learning.Dal.Models
+{
+    public class MongoDBGoodsModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int IDItem { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Learning.Dal/Models/MongoDBOrederList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learning.Dal.Models
+{
+    public class MongoDBOrederList
+    { 
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int IDPurchase { get; set; }
+        public DateTime DateOfOrder { get; set; }
+        public int TotalCost { get; set; }
+    }
+}
